acd261ab84e76547788061d7139051ba02e15388
Michael Pearce <michaelttpearce@gmail.com>
2024-05-20 22:48:48 -0700

    Update model.py

--- mnist/model.py
+++ mnist/model.py
@@ -125,6 +125,8 @@ class MnistModel(nn.Module):
         with torch.no_grad():
             n_correct = 0
             n_samples = 0
+            loss_sum = 0
+            count = 0
             for images, labels in test_loader:
                 images = images.reshape(-1, 28*28).to(self.cfg.device)
                 labels = labels.to(self.cfg.device)
@@ -133,11 +135,14 @@ class MnistModel(nn.Module):
                 _, predicted = torch.max(outputs.data, 1)
                 n_samples += labels.size(0)
                 n_correct += (predicted == labels).sum().item()
+                loss_sum += self.criterion(outputs, labels).item()
+                count += 1
 
             acc = 100.0 * n_correct / n_samples
+            loss = loss_sum / count
             if print_acc:
               print(f'Accuracy on validation set: {acc} %')
-        return acc
+        return acc, loss
 
     def train(self, train_loader, test_loader, optimizer=None, scheduler=None):
         if optimizer is None:
